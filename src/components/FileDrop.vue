37b4ea001988b460ed2b41985ee40ae8de85b434
ClaireCombastel <combastel.cl@gmail.com>
2022-11-25 00:01:10 +0100

    drag and drop

--- src/components/FileDrop.vue
+++ src/components/FileDrop.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
+import {onMounted, onUnmounted, ref } from "vue"
 
+const events = ["dragenter", "dragleave", "dragover", "drop"]
+
+onMounted(() =>{
+  events.forEach(event => document.body.addEventListener(event, (e)=>e.preventDefault()))
+})
+
+onUnmounted(() =>{
+  events.forEach(event => document.body.removeEventListener(event, (e)=>e.preventDefault()))
+})
+
+const emit = defineEmits<{(e:"upload", files:[]):void}>()
+
+const entering = ref(false)
+
+function handleDrop(e: DragEvent) : void {
+  const files = e.dataTransfer?.files as FileList
+  emit("upload", [...files])
+}
+
+function handleInput(e: InputEvent) : void {
+  const files = (e.target as HTMLInputElement).files as FileList
+  emit("upload", [...files])
+}
 </script>
 
 <template>
-  <label v-bind="$attrs" class="label ">
-    <input multiple type="file" class="input">
+  <label v-bind="$attrs" class="label" :class="entering ? 'bg-gray-500' :'bg-gray-100'" @drop.prevent="handleDrop" @dragenter="entering=true" @dragleave="entering=false">
+    <input multiple type="file" class="input" @input="handleInput">
     <span class="message">drop your file here</span>
   </label>
 
@@ -12,7 +36,7 @@
 
 <style scoped>
 .label{
-  @apply relative transition-all ease-in-out duration-300 border border-gray-500;
+  @apply relative transition-all ease-in-out duration-300 border border-gray-500 hover:bg-gray-400;
 }
 .input {
   @apply absolute top-0 left-0 w-full h-full hidden;
